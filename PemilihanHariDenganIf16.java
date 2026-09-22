1107fae78b328bacf514394be2cce41778c06d69
Launa Adhwa <unaadwaa@gmail.com>
2026-09-22 22:37:07 +0700

    Menambahkan PemilihanHariDenganIf16

---
+++ PemilihanHariDenganIf16.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PemilihanHariDenganIf16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int dayNumber;
+
+        System.out.print("Input day number: ");
+        dayNumber = sc.nextInt();
+
+        if (dayNumber >= 1 && dayNumber <= 5) {
+            System.out.println("Weekday");
+        } else if (dayNumber == 6 || dayNumber == 7) {
+            System.out.println("Weekend");
+        } else {
+            System.out.println("Invalid Number");
+        }
+    }
+}
