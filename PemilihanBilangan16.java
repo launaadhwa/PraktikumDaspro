cfb4ad42df175f8f73005784fa3007d372ff6666
Launa Adhwa <unaadwaa@gmail.com>
2026-09-22 21:26:46 +0700

    Memodifikasi PemilihanBilangan16

--- PemilihanBilangan16.java
+++ PemilihanBilangan16.java
@@ -7,13 +7,8 @@ public class PemilihanBilangan16 {
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+       String hasil = (angka % 2 == 0) ? "termasuk bilangan genap" : "termasuk bilangan ganjil";
+
+        System.out.println("Angka " + angka + " " + hasil);
     }
 }
